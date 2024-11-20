19f5b7f322284299c380c4b019199403bfada613
Axel Brisse <axel.brisse@gmail.com>
2024-11-20 07:11:31 +0100

    0017 ok

--- 0017.py
+++ 0017.py
@@ -30,7 +30,7 @@ def naive():
     res = " ".join(
         i for i in map(str, range(10**3, 10**4)) if sum(map(ord, i)) & 1 == 0
     )
-    print("".join(res)[:100])
+    print("".join(res))
 
 
 @timing
@@ -43,7 +43,7 @@ def smart():
             helper(a + 1, 0, 0, ~a & 1)
             return
         if c >= 10:
-            helper(a, b + 1, 0, (a ^ b) & 1)
+            helper(a, b + 1, 0, ~(a ^ b) & 1)
             return
         if d >= 10:
             helper(a, b, c + 1, ~(a ^ b ^ c) & 1)
@@ -58,12 +58,42 @@ def smart():
     i = 0
     res = [32] * (4500 * 5 - 1)
     helper(1, 0, 0, 1)
-    print(bytearray(res).decode("ascii")[:100])
+    print(bytearray(res).decode("ascii"))
+
+
+@timing
+def genius():
+    size = 4500 * 5 - 1
+    res = [32] * size
+    i = 0
+    for a in range(49, 58):
+        for b in range(48, 58):
+            for c in range(48, 58):
+                start = ((a ^ b ^ c) & 1) ^ 48
+                res[i] = res[i + 5] = res[i + 10] = res[i + 15] = res[i + 20] = a
+                res[i + 1] = res[i + 6] = res[i + 11] = res[i + 16] = res[i + 21] = b
+                res[i + 2] = res[i + 7] = res[i + 12] = res[i + 17] = res[i + 22] = c
+                res[i + 3] = start
+                res[i + 8] = start + 2
+                res[i + 13] = start + 4
+                res[i + 18] = start + 6
+                res[i + 23] = start + 8
+                i += 25
+    print(bytearray(res).decode("ascii"))
+
+
+def test0017(capfd):
+    naive()
+    naive_output = capfd.readouterr().out.strip()
+    smart()
+    smart_output = capfd.readouterr().out.strip()
+    genius()
+    genius_output = capfd.readouterr().out.strip()
+    assert naive_output == smart_output == genius_output
 
 
 def main():
-    naive()
-    smart()
+    genius()
 
 
 if __name__ == "__main__":
